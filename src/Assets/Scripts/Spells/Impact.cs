1ccc70f97dc1f3f5a6cb468b14add0ba00bd522c
Paul Davis <pdavis86@hotmail.co.uk>
2021-01-02 17:43:37 +0000

    Progress

---
+++ src/Assets/Scripts/Spells/Impact.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Spells;
+using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    public class Impact : SpellBehaviourBase
+    {
+        private void Update()
+        {
+            UpdateTimeAlive(gameObject);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            collision.rigidbody.AddExplosionForce(400f, collision.transform.position, 5f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ src/Assets/Scripts/Spells/SpellBehaviourBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    public abstract class SpellBehaviourBase : MonoBehaviour
+    {
+        const float _timeToLive = 3f;
+        private float _timeAlive;
+
+        internal void UpdateTimeAlive(GameObject spell)
+        {
+            _timeAlive += Time.deltaTime;
+            if (_timeAlive >= _timeToLive)
+            {
+                Destroy(spell);
+            }
+        }
+    }
+}
