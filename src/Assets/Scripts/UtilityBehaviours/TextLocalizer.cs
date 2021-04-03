d8c621dcfef725959eb96fc281c6b3d37a2df8ce
Paul Davis <pdavis86@hotmail.co.uk>
2021-04-03 16:46:22 +0100

    Added TextLocalizer

---
+++ src/Assets/Scripts/UtilityBehaviours/TextLocalizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UtilityBehaviours
+{
+    [RequireComponent(typeof(Text))]
+    public class TextLocalizer : MonoBehaviour
+    {
+        public string id;
+
+        void Start()
+        {
+            GetComponent<Text>().text = ResolveStringValue(id);
+        }
+
+        void OnValidate()
+        {
+            GetComponent<Text>().text = ResolveStringValue(id);
+        }
+
+        public string ResolveStringValue(string id)
+        {
+            //todo: based on set language and ID, return the translation
+            return id;
+        }
+
+    }
+}
