fbdb898e15487fea0bfe3c822414b7a9774d2378
Paul Davis <pdavis86@hotmail.co.uk>
2021-04-03 22:43:13 +0100

    Tidying

--- src/Assets/Scripts/PlayerBehaviours/PlayerSetup.cs
+++ src/Assets/Scripts/PlayerBehaviours/PlayerSetup.cs
@@ -171,7 +171,7 @@ public class PlayerSetup : NetworkBehaviour
 
     private void LoadFromPlayerData(PlayerData loadData)
     {
-        //todo:
+        //todo: set texture URI
         _textureUri = string.Empty;
         _loadWasSuccessful = _inventory.ApplyChanges(loadData.Inventory, true);
     }
--- src/Assets/Scripts/UI/SettingsMenuUi.cs
+++ src/Assets/Scripts/UI/SettingsMenuUi.cs
@@ -6,26 +6,26 @@ public class SettingsMenuUi : MonoBehaviour
 {
     //void Start()
     //{
-    //    //if (!string.IsNullOrWhiteSpace(GameManager.Instance.PlayerSkinUrl))
-    //    //{
-    //    //    string filePath;
-    //    //    if (!GameManager.Instance.PlayerSkinUrl.StartsWith("http"))
-    //    //    {
-    //    //        //todo: upload file?
-    //    //        filePath = GameManager.Instance.PlayerSkinUrl;
-    //    //    }
-    //    //    else
-    //    //    {
-    //    //        //todo: download file
-    //    //        filePath = GameManager.Instance.PlayerSkinUrl;
-    //    //    }
+    //    if (!string.IsNullOrWhiteSpace(GameManager.Instance.PlayerSkinUrl))
+    //    {
+    //        string filePath;
+    //        if (!GameManager.Instance.PlayerSkinUrl.StartsWith("http"))
+    //        {
+    //            //todo: upload file?
+    //            filePath = GameManager.Instance.PlayerSkinUrl;
+    //        }
+    //        else
+    //        {
+    //            //todo: download file
+    //            filePath = GameManager.Instance.PlayerSkinUrl;
+    //        }
 
-    //    //    if (System.IO.File.Exists(filePath))
-    //    //    {
-    //    //        SetPlayerTexture(filePath);
-    //    //        TextureUri = filePath;
-    //    //    }
-    //    //}
+    //        if (System.IO.File.Exists(filePath))
+    //        {
+    //            SetPlayerTexture(filePath);
+    //            TextureUri = filePath;
+    //        }
+    //    }
     //}
 
 }
