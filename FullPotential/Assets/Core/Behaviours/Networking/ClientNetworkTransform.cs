d6c2c7a9c3b5544e3eef7841e355f4183cf02a30
Paul Davis <pdavis86@hotmail.co.uk>
2022-01-22 15:27:27 +0000

    Added NetworkTransform code for later use

---
+++ FullPotential/Assets/Core/Behaviours/Networking/ClientNetworkTransform.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace FullPotential.Core.Behaviours.Networking
+{
+    //Netcode for GameObjects way to handle client-side transform data
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+
+            CanCommitToTransform = IsOwner;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (
+                NetworkManager == null
+                || (!NetworkManager.IsConnectedClient && !NetworkManager.IsListening)
+                || !CanCommitToTransform)
+            {
+                return;
+            }
+
+            TryCommitTransformToServer(transform, NetworkManager.LocalTime.Time);
+        }
+    }
+}
