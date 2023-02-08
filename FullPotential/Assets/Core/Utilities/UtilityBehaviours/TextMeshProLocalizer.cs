a07f4ec2235c6aad44b2b73c30de49ba58cb67fc
Paul Davis <pdavis86@hotmail.co.uk>
2023-02-08 22:15:58 +0000

    Added localizer for TMP inputs

---
+++ FullPotential/Assets/Core/Utilities/UtilityBehaviours/TextMeshProLocalizer.cs
@@ -0,0 +1,38 @@
+﻿using FullPotential.Api.Ioc;
+using FullPotential.Api.Localization;
+using FullPotential.Api.Utilities.Extensions;
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable once UnusedType.Global
+
+namespace FullPotential.Core.Utilities.UtilityBehaviours
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TextMeshProLocalizer : MonoBehaviour
+    {
+        private TextMeshProUGUI _textComponent;
+        private ILocalizer _localizer;
+
+        // ReSharper disable once UnassignedField.Global
+        public string TranslationId;
+
+        // ReSharper disable once UnusedMember.Local
+        private void Awake()
+        {
+            _textComponent = GetComponent<TextMeshProUGUI>();
+            _localizer = DependenciesContext.Dependencies.GetService<ILocalizer>();
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void OnEnable()
+        {
+            if (TranslationId.IsNullOrWhiteSpace())
+            {
+                Debug.LogWarning($"Missing {nameof(TranslationId)} on {gameObject.name} under {transform.parent.gameObject.name}");
+            }
+
+            _textComponent.text = _localizer.Translate(TranslationId);
+        }
+    }
+}
